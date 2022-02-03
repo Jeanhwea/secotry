5a9264f1a8eba98b1e21839082ee05e8d13ffb2e
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-02-04 00:41:04 +0800

    add: 添加排序的逻辑

--- ch01_array/lc004.go
+++ ch01_array/lc004.go
@@ -1,5 +1,7 @@
 package ch01_array
 
+import "math"
+
 func findMedianSortedArrays_01(a []int, b []int) float64 {
 	total := len(a) + len(b)
 	if total%2 == 0 {
@@ -65,10 +67,38 @@ func findMedianSortedArrays(a []int, b []int) float64 {
 	if n > m {
 		return findMedianSortedArrays(b, a)
 	}
-	left, right := 0, m
-	median1, median2 := 0, 0
-	for left <= right {
-		i := (left + right) / 2
+	beg, end, leftMaxVal, rightMinVal := 0, n, 0, 0
+	for beg <= end {
+		// i+j == (m+n+1)/2 使得 a[0..i] + b[0..j] 刚好是 a[..] 并 b[..] 总元素的一半
+		i := (beg + end) / 2
 		j := (m+n+1)/2 - i
+
+		// 记录区间边界值 x0, x1, y0, y1 => a[i-1], a[i], b[j-1], b[j]
+		x0, x1, y0, y1 := math.MinInt64, math.MaxInt64, math.MinInt64, math.MaxInt64
+		if i != 0 {
+			x0 = a[i-1]
+		}
+		if i != n {
+			x1 = a[i]
+		}
+		if j != 0 {
+			y0 = b[j-1]
+		}
+		if j != m {
+			y1 = b[j]
+		}
+
+		// leftMaxVal 是左半部分的最大值， rightMinVal 是右半部分的最小值
+		if x0 <= y1 {
+			leftMaxVal, rightMinVal = max(x0, y0), min(x1, y1)
+			beg = i + 1
+		} else {
+			end = i - 1
+		}
 	}
+
+	if (n+m)%2 == 0 {
+		return float64(leftMaxVal+rightMinVal) / 2
+	}
+	return float64(leftMaxVal)
 }
