6b05aa381e754fe42d3679790611a740507891ac
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-02-06 02:57:00 +0800

    update

--- ch01_array/lc060_test.go
+++ ch01_array/lc060_test.go
@@ -3,6 +3,11 @@ package ch01_array
 import "testing"
 
 func Test_LC060_01(t *testing.T) {
-	ans := getPermutation(4, 3)
+	ans := getPermutation(4, 13)
+	t.Logf("ans = %v", ans)
+}
+
+func Test_LC060_02(t *testing.T) {
+	ans := getPermutation(4, 24)
 	t.Logf("ans = %v", ans)
 }
