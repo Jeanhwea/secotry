3b1f7dd42069245c71ff0ff784849be5ca550780
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-02-06 02:55:45 +0800

    add: 添加排列的逻辑②

--- ch01_array/lc060.go
+++ ch01_array/lc060.go
@@ -1,8 +1,35 @@
 package ch01_array
 
+import "strconv"
+
 // Cantor Expansion
 // k-1 = an*(n-1)! + ... + a2*2! + a1
 func getPermutation(n int, k int) string {
+	fact := make([]int, n+1)
+	fact[0] = 1
+	for i := 1; i <= n; i++ {
+		fact[i] = i * fact[i-1]
+	}
+	k--
+
 	ans := ""
+	valid := make([]int, n+1)
+	for i := 0; i <= n; i++ {
+		valid[i] = 1
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		order := k/fact[i] + 1
+		for j := 1; j <= n; j++ {
+			order -= valid[j]
+			if order == 0 {
+				ans += strconv.Itoa(j)
+				valid[j] = 0
+				break
+			}
+		}
+		k %= fact[i]
+	}
+
 	return ans
 }
