5c372b9a5096215902efe43295133b4dfacd2d41
Jinghui Hu <hujinghui.jeffrey@bytedance.com>
2022-02-06 02:39:51 +0800

    add: 添加排列的逻辑

---
+++ ch01_array/lc060.go
@@ -0,0 +1,8 @@
+package ch01_array
+
+// Cantor Expansion
+// k-1 = an*(n-1)! + ... + a2*2! + a1
+func getPermutation(n int, k int) string {
+	ans := ""
+	return ans
+}
---
+++ ch01_array/lc060_test.go
@@ -0,0 +1,8 @@
+package ch01_array
+
+import "testing"
+
+func Test_LC060_01(t *testing.T) {
+	ans := getPermutation(4, 3)
+	t.Logf("ans = %v", ans)
+}
